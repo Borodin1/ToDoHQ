6b8213e766d25c74422247c67311e9ee592e5385
Alexander Borodin <borodinag1902@gmail.com>
2025-04-23 17:55:05 +0300

    fix:changing in not-found componnet

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -2,9 +2,11 @@ import { Link } from "react-router-dom";
 
 export const NotFoundPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-stretch ">
-      <h2 className='text-5xl'>Your visited page not found.<br/>You may go home page.</h2>
-      <Link to="/" className='text-sky-500'>Back to home page</Link>
+    <div className="h-screen flex items-center justify-center">
+      <div className='flex flex-col justify-center items-center gap-2.5'>
+      <h2 className='text-5xl font-semibold'>Your visited page not found.<br/>You may go home page.</h2>
+      <Link to="/" className='text-sky-500 font-medium'>Back to home page</Link>
+      </div>
     </div>
   );
 };
